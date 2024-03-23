7223e63597a0d2dfc230417c35d9e5b449275ce1
Omkar Kumbhar <omkar.kumbhar@ftbcolab.com>
2024-03-23 23:36:06 +0530

    initial commit

---
+++ portfolio/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div>
+      <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem] ">
+                <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]"></div>
+            </div>
+        <h1 className="">Omkar Kumbhar</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
